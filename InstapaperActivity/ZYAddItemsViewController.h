6176756f8124fb9bec3952ab4feff758eea0cb08
Mariano Abdala <mariano@zerously.com>
2012-10-06 14:16:10 -0300

    Improved the navigation. Support for multiple items.

---
+++ InstapaperActivity/ZYAddItemsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZYtemsSelectorViewController.h
+//  InstapaperActivity
+//
+//  Created by Mariano Abdala on 10/6/12.
+//  Copyright (c) 2012 Zerously. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZYtemsSelectorViewController : UIViewController
+
+@end
---
+++ InstapaperActivity/ZYAddItemsViewController.m
@@ -0,0 +1,38 @@
+//
+//  ZYtemsSelectorViewController.m
+//  InstapaperActivity
+//
+//  Created by Mariano Abdala on 10/6/12.
+//  Copyright (c) 2012 Zerously. All rights reserved.
+//
+
+#import "ZYtemsSelectorViewController.h"
+
+@interface ZYtemsSelectorViewController ()
+
+@end
+
+@implementation ZYtemsSelectorViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
